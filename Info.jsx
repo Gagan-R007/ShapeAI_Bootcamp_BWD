6d9755001bd38da109d247b229dc4aedfcc9b490
Gagan-R007 <86520915+Gagan-R007@users.noreply.github.com>
2021-06-26 18:01:24 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Web Development</h1>
+      <p>A basic web development React Js Bootcamp taken by Shaurya Sinha </p>
+    </div>
+  );
+}
+
+export default Info;
